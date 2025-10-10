d76838e7910cef0a2e141a2db23809bf5f77d32c
MarioMaker404 <147083364+GenericRoblox@users.noreply.github.com>
2025-10-09 20:06:13 -0700

    Add files via upload

---
+++ Kellan-Neiman-com-V2/script.js
@@ -0,0 +1,24 @@
+document.getElementById("btn-vb").addEventListener("click", () => {
+  document.getElementById("mark-vb").scrollIntoView({
+    behavior: "smooth",
+    block: "start"
+  });
+});
+document.getElementById("btn-coding").addEventListener("click", () => {
+  document.getElementById("mark-coding").scrollIntoView({
+    behavior: "smooth",
+    block: "start"
+  });
+});
+document.getElementById("btn-engineering").addEventListener("click", () => {
+  document.getElementById("mark-engineering").scrollIntoView({
+    behavior: "smooth",
+    block: "start"
+  });
+});
+document.getElementById("btn-chess").addEventListener("click", () => {
+  document.getElementById("mark-chess").scrollIntoView({
+    behavior: "smooth",
+    block: "start"
+  });
+});
